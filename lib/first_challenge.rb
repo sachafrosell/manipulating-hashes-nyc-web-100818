3b4915bc3d50b002a7d2904d744aaf93060cb408
Alexander Frosell <sachafrosell@gmail.com>
2018-10-04 19:50:43 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,9 @@ def first_challenge
   #your code here
   contacts.each do |key, value|
     value.each do |attribute, answer|
-      if attribute == 
+      if attribute == :favorite_icecream_flavors
+        answer.each do |flavor|
+          puts "flavor"
 
   #remember to return your newly altered contacts hash!
   contacts
