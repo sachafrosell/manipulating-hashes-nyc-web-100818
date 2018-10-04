4d06df9e3fe9149b23f59f8e8a8219178895f5bd
Alexander Frosell <sachafrosell@gmail.com>
2018-10-04 19:58:26 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,7 +12,7 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-  contacts["Freddy Mercury"][:favorite_icecream_flavors]
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete("strawberry")
  
   contacts
 end
